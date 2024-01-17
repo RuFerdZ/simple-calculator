a326458b862615b0bfec670a499bd32fe87918ee
RuFerdZ <ge.rusiru@gmail.com>
2024-01-17 03:17:50 -0500

    initial commit

---
+++ src/main/java/com/backend/calculator/CalculatorController.java
@@ -0,0 +1,29 @@
+package com.backend.calculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
+
+    @GetMapping("/subtract")
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return a - b;
+    }
+
+    @GetMapping("/multiply")
+    public int multiply(@RequestParam int a, @RequestParam int b) {
+        return a * b;
+    }
+
+    @GetMapping("/divide")
+    public int divide(@RequestParam int a, @RequestParam int b) {
+        return a / b;
+    }
+}
