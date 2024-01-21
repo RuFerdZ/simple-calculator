f09a9bcdcefa5220a6369069f849f845145feaac
RuFerdZ <ge.rusiru@gmail.com>
2024-01-21 02:16:14 -0500

    added tests

--- src/main/java/com/backend/calculator/CalculatorController.java
+++ src/main/java/com/backend/calculator/CalculatorController.java
@@ -1,10 +1,12 @@
 package com.backend.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calc")
 public class CalculatorController {
 
     @GetMapping("/add")
